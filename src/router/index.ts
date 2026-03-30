7950edfa9914d9f4ceaff74c4762071a9b9b7697
lucasrebl <rebouletlucas@gmail.com>
2026-03-30 17:00:10 +0200

    changement des titres des pages et Ajout d'une icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,30 +4,48 @@ import BinaireView from '@/views/binaire/BinaireView.vue'
 import QrcodeView from '@/views/qrcode/QrcodeView.vue'
 import PasswordView from '@/views/password/PasswordView.vue'
 
+const defaultTitle = 'ToolBoxX | Outils rapides du quotidien'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'ToolBoxX | Accueil',
+      },
     },
     {
       path: '/binaire',
       name: 'binaire',
-      component: BinaireView
+      component: BinaireView,
+      meta: {
+        title: 'ToolBoxX | Convertisseur binaire',
+      },
     },
     {
       path: '/qrcode',
       name: 'qrcode',
-      component: QrcodeView
+      component: QrcodeView,
+      meta: {
+        title: 'ToolBoxX | QR Code',
+      },
     },
     {
       path: '/password',
       name: 'password',
-      component: PasswordView
-    }
+      component: PasswordView,
+      meta: {
+        title: 'ToolBoxX | Generateur de mot de passe',
+      },
+    },
   ],
+})
+
+router.afterEach((to) => {
+  document.title = typeof to.meta.title === 'string' ? to.meta.title : defaultTitle
 })
 
 export default router
